63d499670ecfa674b5758606064ad003e57cfb92
LongAero <bingus2406@gmail.com>
2025-03-11 10:02:35 -0600

    Update LCSDATA.m

--- LCSDATA.m
+++ LCSDATA.m
@@ -1,15 +1,22 @@
 function [theta_exp, w_exp, v_exp, time] = LCSDATA(filename)
+%152.5
     DATA = readmatrix(filename);
-    n = floor(DATA(1,2)/360);
+    n = floor(DATA(1,2)/360) + 1;
     DATA(:,2) = DATA(:,2) - n*360;
+    for j = 1:length(DATA)
+        if(DATA(j,2) >= 0) % + DATA(1,2)
+            startbound = j;
+            break
+        end
+    end
     for i = 1:length(DATA)
         if(DATA(i,2) >= 6*360) % + DATA(1,2)
             endbound = i;
             break
         end
     end
-    theta_exp = DATA(1:endbound,2);
-    w_exp = DATA(1:endbound,4);
-    v_exp = DATA(1:endbound,5);
-    time = DATA(1:endbound,1);
+    theta_exp = DATA(startbound:endbound,2);
+    w_exp = DATA(startbound:endbound,4);
+    v_exp = DATA(startbound:endbound,5);
+    time = DATA(startbound:endbound,1);
 end
