40d97ecd07a1335f541f5152bdaf059699d5ea8d
LongAero <bingus2406@gmail.com>
2025-03-11 10:02:21 -0600

    Update LCSMODEL.m

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,8 +1,7 @@
-%[v_mod] = LCSMODEL(r, d, l, theta, w)
-%[theta_exp, w_exp, v_exp, time] = LSCDATA(filename)
+
 
 function [v_mod] = LCSMODEL(r, d, l, theta, w)
     theta = theta / 180 * pi;
-    v_mod = - w .* r .* sin(theta) - r .* tan(asin((d - r * sin(theta))/l)) .* cos(theta) .* w;
+    v_mod = - w / 180 * pi .* r .* sin(theta) - r .* tan(asin((d - r * sin(theta))/l)) .* cos(theta) .* w / 180 * pi;
 end
 
