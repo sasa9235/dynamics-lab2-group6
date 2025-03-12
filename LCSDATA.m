719fd90655b3d3b67a00c815d61938e6644b5ba3
LongAero <bingus2406@gmail.com>
2025-03-12 09:46:54 -0600

    Update LCSDATA.m

--- LCSDATA.m
+++ LCSDATA.m
@@ -1,20 +1,26 @@
+%% LCSDATA Splice experimental data to data for 6 full revolution and start at theta = 0
 function [theta_exp, w_exp, v_exp, time] = LCSDATA(filename)
-%152.5
+% read in data as a matrix
     DATA = readmatrix(filename);
+% calculate n of full revolution to remove
     n = floor(DATA(1,2)/360) + 1;
+% subtract n full revolution from theta collum
     DATA(:,2) = DATA(:,2) - n*360;
+% calculate start bound index for DATA crop for theta to start at ~0
     for j = 1:length(DATA)
         if(DATA(j,2) >= 0) % + DATA(1,2)
             startbound = j;
             break
         end
     end
+% calculate end bound index for DATA crop for 6 full revolution
     for i = 1:length(DATA)
-        if(DATA(i,2) >= 6*360) % + DATA(1,2)
+        if(DATA(i,2) >= 6*360)
             endbound = i;
             break
         end
     end
+% split data matrix into theta, w, v, time and crop data to start/end bound
     theta_exp = DATA(startbound:endbound,2);
     w_exp = DATA(startbound:endbound,4);
     v_exp = DATA(startbound:endbound,5);
