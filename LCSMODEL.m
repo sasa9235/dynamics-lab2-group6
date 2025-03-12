f8b46340402e230a916dc00e3f729796341a9b44
LongAero <bingus2406@gmail.com>
2025-03-12 09:46:42 -0600

    Update LCSMODEL.m

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,7 +1,8 @@
-
-
+%% LCSMODEL output ideal velocity of Collar (v_b)
 function [v_mod] = LCSMODEL(r, d, l, theta, w)
+    % convert theta vector from degrees to radian
     theta = theta / 180 * pi;
+    % calculate v_mod according to model derivation
     v_mod = - w / 180 * pi .* r .* sin(theta) - r .* tan(asin((d - r * sin(theta))/l)) .* cos(theta) .* w / 180 * pi;
 end
 
